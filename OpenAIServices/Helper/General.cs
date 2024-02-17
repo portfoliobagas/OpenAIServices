7363a41d2eaf03f9d822515d20088d7ab76803b2
Bagas Wijaya <portfoliobagas@gmail.com>
2024-02-17 15:36:46 +0700

    Inirial General.cs

---
+++ OpenAIServices/Helper/General.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenAIServices.Helper
+{
+    public class General
+    {
+    }
+
+    public static class Constan
+    {
+        public const string STR_KEY_OPENAI = "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";
+        public const string STR_MODEL_GPT = "gpt-3.5-turbo-instruct";
+    }
+}
