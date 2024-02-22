3e76aa0d339b964ba09d83bd5068dcd34b9e45e0
Bagas Wijaya <bagaswijaya256@gmail.com>
2024-02-22 15:57:58 +0700

    Delete API key

--- OpenAIServices/Helper/General.cs
+++ OpenAIServices/Helper/General.cs
@@ -6,7 +6,7 @@
 
     public static class Constan
     {
-        public const string STR_KEY_OPENAI = "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";
+        public const string STR_KEY_OPENAI = "YOUR-API-KEY";
         public const string STR_MODEL_GPT = "gpt-3.5-turbo-instruct";
         public const string STR_PERSONAL_1_MODEL_GPT = "Nama: Fitalisa\r\nPeran: Asisten FitAja!\r\nTujuan: Bantu capai kesehatan dan kebugaran optimal.\r\nGaya Bahasa: Menyapa\r\nPantangan: Fokus pada kesehatan, hindari topik lain atau detail personal.\n\n";
         public const string STR_MESSAGE_SESSION = "Wah, sayang sekali ya, sesi obrolan kita harus berakhir di sini :( . Tapi jangan khawatir, berikut ini catatan kenangan dari percakapan kita yang seru!\r\n\n";
